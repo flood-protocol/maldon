ee97d3e04c4def8b45856ba8064f2a1ed557f7d5
Francesco Iannelli <f.iannelli.francesco229@gmail.com>
2023-07-10 13:29:48 +0200

    Add better error handling and refactor
    Signed-off-by: Francesco Iannelli <f.iannelli.francesco229@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,29 @@
-use std::{ops::Deref, str::FromStr};
+use std::{error, fmt, ops::Deref, str::FromStr};
 
-use alloy_primitives::{Address, FixedBytes};
 use clap::Parser;
 
+use alloy_primitives::{Address, FixedBytes};
+
+/// Pattern.
 #[derive(Debug, Clone)]
 pub struct Pattern(Box<str>);
+
+impl Pattern {
+    /// Returns the byte representation of the pattern. Pads the pattern with 0s if it is uneven.
+    ///
+    /// # Errors
+    ///
+    /// Returns errors if the conversion fails.
+    pub(crate) fn into_bytes(self) -> Result<Vec<u8>, hex::FromHexError> {
+        let mut string = self.to_string();
+
+        if self.len() % 2 != 0 {
+            string += "0"
+        };
+
+        hex::decode(string)
+    }
+}
 
 impl Deref for Pattern {
     type Target = str;
@@ -14,17 +33,39 @@ impl Deref for Pattern {
     }
 }
 
+/// Pattern errors.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PatternError {
+    /// The pattern's length exceeds 39 characters or the pattern is empty.
+    InvalidPatternLength,
+    /// The patters is not in hexadecimal format.
+    NonHexPattern,
+}
+
+impl fmt::Display for PatternError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PatternError::InvalidPatternLength => write!(f, "invalid length"),
+            PatternError::NonHexPattern => write!(f, "pattern must be hex digits only"),
+        }
+    }
+}
+
+impl error::Error for PatternError {}
+
 impl FromStr for Pattern {
-    type Err = &'static str;
+    type Err = PatternError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.len() >= 40 || s.is_empty() {
-            Err("Invalid length.")
-        } else if s.chars().all(|c| c.is_ascii_hexdigit()) {
-            Ok(Self(s.into()))
-        } else {
-            Err("Pattern must be hex digits only.")
+            return Err(PatternError::InvalidPatternLength);
         }
+
+        if s.chars().any(|c| !c.is_ascii_hexdigit()) {
+            return Err(PatternError::NonHexPattern);
+        }
+
+        Ok(Self(s.into()))
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ mod cli;
 mod mine;
 
 use clap::Parser;
+
 use cli::Config;
+
 use mine::Miner;
 
 fn main() {
     let config = Config::parse();
 
-    let mut miner = Miner::new(config);
+    let miner = Miner::new(config);
+
     let (address, salt) = miner.mine();
+
     println!("Found salt {salt:?} ==> {address:?}");
 }
